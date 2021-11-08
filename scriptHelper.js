30185a2b0c6477c6a4603275da7cd3a55638f884
pallavi <pallavimatkar26@gmail.com>
2021-11-08 14:19:04 -0600

    first commit

--- scriptHelper.js
+++ scriptHelper.js
@@ -1,34 +1,40 @@
 // Write your helper functions here!
 require('isomorphic-fetch');
 
-function addDestinationInfo(document, name, diameter, star, distance, moons, imageUrl) {
+function addDestinationInfo( name, diameter, star, distance, moons, imageUrl) {
+   
    // Here is the HTML formatting for our mission target div.
-   /*
-                <h2>Mission Destination</h2>
-                <ol>
-                    <li>Name: </li>
-                    <li>Diameter: </li>
-                    <li>Star: ${star}</li>
-                    <li>Distance from Earth: </li>
-                    <li>Number of Moons: </li>
-                </ol>
-                <img src="">
-   */
-}
-
-function validateInput(testInput) {
+   
+   document.getElementById("missionTarget").innerHTML = `
+         <h2>Mission Destination</h2>
+         <ol>
+             <li>Name: ${name}</li>
+             <li>Diameter: ${diameter}</li>
+             <li>Star: ${star}</li>
+             <li>Distance from Earth: ${distance}</li>
+             <li>Number of Moons: ${moons}</li>
+         </ol>
+         <img src="${imageUrl}">
+        ` 
    
 }
 
-function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
+function validateInput(e) {
+   
+   
+    
+}
+
+ function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
    
 }
 
 async function myFetch() {
     let planetsReturned;
 
-    planetsReturned = await fetch().then( function(response) {
-        });
+    planetsReturned = await fetch("https://handlers.education.launchcode.org/static/planets.json").then( function(response) {
+           return response.json()
+         });
 
     return planetsReturned;
 }
@@ -41,3 +47,5 @@ module.exports.validateInput = validateInput;
 module.exports.formSubmission = formSubmission;
 module.exports.pickPlanet = pickPlanet; 
 module.exports.myFetch = myFetch;
+
+
